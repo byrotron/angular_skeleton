ceaece76a2d1a7ecad7386a4f7f212b8184fde72
Byron Georgiou <byron@isarray.com>
2017-10-17 13:20:37 -0400

    Ensure action bar is hidden

--- action-bar/actionbar-toggle.ts
+++ action-bar/actionbar-toggle.ts
@@ -10,13 +10,15 @@ export const ActionbarToggle =  trigger("slidetoggle", [
   state('hide',
     style({
       transform: "translateY(100%)",
-      height: 0
+      height: 0,
+      display: 'none'
     })
   ),
   state('show',
     style({
       transform: "translateY(0)",
-      height: 'auto'
+      height: 'auto',
+      display: 'initial'
     })
   ),
   transition('show=>hide', [
