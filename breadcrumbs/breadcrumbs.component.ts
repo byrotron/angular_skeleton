e51af337cf81f8d58e5fb255441664a1003ac6eb
Byron Georgiou <byron@isarray.com>
2018-04-18 21:12:57 -0400

    Updated breadcrumbs to make it more dynamic

--- breadcrumbs/breadcrumbs.component.ts
+++ breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { BreadcrumbService } from 'pangular/breadcrumbs/breadcrumb.service';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'sktn-breadcrumbs',
@@ -8,32 +10,42 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class SktnBreadcrumbsComponent  {
 
-  urls: string[] = [];
-
   constructor(
-    public router: Router
+    protected router: Router,
+    public breadcrumbs: BreadcrumbService
   ) { }
 
   ngOnInit() {
-    this.router.events.forEach((event) => {
 
+    this.generateBreadcrumbs(this.router.url)
+
+    this.router.events.forEach((event) => {
       if(event instanceof NavigationEnd) {
-        this.urls = event.url.split('/').splice(1);
+        this.generateBreadcrumbs(event.url)
       }
 
+    });
+
+  }
+
+  generateBreadcrumbs(urls: any) {
+    let url_arr = urls.split('/').splice(1)
+    this.breadcrumbs.urls = url_arr.map((string: string) => {
+      return {
+        name: this.breadcrumbs.mappings[string.toLowerCase()] ? this.breadcrumbs.mappings[string.toLowerCase()] : string,
+        url: string
+      }
     });
   }
 
   breadcrumbNavigator(ind: number) {
     
-    if(this.urls.length > 0) {
-      this.urls.splice(ind + 1);
-      this.router.navigate(this.urls);
-    } else {
-      this.router.navigate(['/admin']);
-    }
+    this.breadcrumbs.urls.splice(ind + 1);
+    let urls = this.breadcrumbs.urls.map((item: any) => {
+      return item.url;
+    })
+    this.router.navigate(urls);
     
   }
-
 
 }
