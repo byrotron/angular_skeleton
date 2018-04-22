3368927887eafcdc7aa6672755d095797740a469
Byron Georgiou <byron@isarray.com>
2018-04-22 14:19:00 -0400

    Added more validations to validators class

--- app/validators.ts
+++ app/validators.ts
@@ -8,4 +8,46 @@ export class SktnValidators {
     return !isNaN(+control.value) ? null : { number: true } 
   }
 
+  matches(matched_control: string): ValidationErrors|null {
+
+    return (control: AbstractControl): ValidationErrors | null => {
+      let match = control.root.get(matched_control);
+      console.log("Test");
+      return match && control.value === match.value ? null : { match: 'Values do not match' } 
+    };
+
+  }
+
+  password(control: AbstractControl): ValidationErrors|null {
+
+    let alpha = new RegExp('[A-z]+');
+    let numeric = new RegExp('[0-9]+');
+    let symbols = new RegExp('[!@#$%^&*()-\+]+');
+    let val = control.value;
+    let errors = ['password is too weak'];
+
+    if(val && alpha.test(val) && numeric.test(val) && symbols.test(val) && val.length >= 8) {
+      return null;
+    } else {
+
+      if(!alpha.test(val)) {
+        errors.push('must contain letters')
+      }
+
+      if(!numeric.test(val)) {
+        errors.push('must contain numbers')
+      } 
+
+      if(!symbols.test(val)) {
+        errors.push('must contain symbols')
+      }
+
+      if(!val || val.length < 8) {
+        errors.push('must be longer than 8 chars')
+      }
+
+      return {'password': errors}; 
+    }
+  }
+
 }
